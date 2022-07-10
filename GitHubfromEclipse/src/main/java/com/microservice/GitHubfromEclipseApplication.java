df25de0d379e943f4d62ae3c6c6c0433187bd94d
upendergupta <upender.gupta1991@gmail.com>
2022-07-10 20:01:38 +0530

    second commit

--- GitHubfromEclipse/src/main/java/com/microservice/GitHubfromEclipseApplication.java
+++ GitHubfromEclipse/src/main/java/com/microservice/GitHubfromEclipseApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitHubfromEclipseApplication {
 
 	public static void main(String[] args) {
+		
+		System.out.println("Ram");
 		SpringApplication.run(GitHubfromEclipseApplication.class, args);
 	}
 
